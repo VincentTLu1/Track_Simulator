c36521cfb7149571a8ad8b6b43633b63572ad5b9
Vincent Lu <vincenttlu2005@gmail.com>
2026-04-04 23:32:34 -0700

    Added feature for user to make track

--- Track_Simulator.py
+++ Track_Simulator.py
@@ -2,6 +2,7 @@ import math
 import pandas as pd
 import Segment_Builder as segments
 
+# Givens
 coefficient_of_friction = 0.7 #Adjust accordingly
 coefficient_of_gravity = 9.81
 max_acceleration = 14 #Adjust accordingly for the max acceleration of the car in question
@@ -88,6 +89,7 @@ def build_track():
     ]
     return track
 
+# Computes the total time on the track accounting for the straight and corner
 def compute_track_time(track):
     v_current = 0
     total_time = 0
@@ -143,11 +145,51 @@ def create_segment_time_dict(track):
 
     return all_segment_times
     
-#Might not need this
-# def clean_segment_values(segment_time_list):
-#     updated_segment_time_list = [f"{x:.3f}" for x in segment_time_list]
-#     return updated_segment_time_list
+#User specifies their own type of track
+def build_custom_track_for_user():
+    flag = False
+    user_track = []
+    length = None
+    radius = None
+    name = None
+
+    while flag == False:
+        select = input("Do you want straight or corner? (S/C)")
+        if select.lower() == "s":
+            length = input("Type in your length: ")
+            name = input("Type in the name (S1/C1): ")
+            straight = segments.Straight(int(length), name)
+            user_track.append(straight.to_dict())
+        elif select.lower() == "c":
+            radius = input("Type in your radius: ")
+            string_angle = input("Type in your angle (90/180/360): ")
+            name = input("Type in the name (S1/C1): ")
+            picked_angle = pick_angle(string_angle)
+            corner = segments.Corner(int(radius), picked_angle, name)
+            user_track.append(corner.to_dict())
+        
+        choice = input("Are you done? (Y/N)")
+        if (choice.lower() == "y"):
+            flag = True
+        else:
+            flag = False
     
+    return user_track
+
+
+#Converts the angle into something the program can understand
+def pick_angle(angle):
+    selected_angle = None
+    if angle == "90":
+        selected_angle = degree_90
+    elif angle == "180":
+        selected_angle == degree_180
+    else:
+        selected_angle == degree_360
+    return selected_angle
+        
+
+#Prints the results into a table in which the user can understand
 def print_results(segment_times, total_time):
     labels = list(segment_times.keys()) + ["Total"]
     values = list(segment_times.values()) + [total_time]
@@ -158,27 +200,36 @@ def print_results(segment_times, total_time):
 
     
 
-#This is a test for the repo
+
 def main():
     """Main function of the script."""
     # Your main program logic goes here
     print("This code runs when the script is executed directly.")
-    test = []
-    # a = build_track()
+    # test = []
+    
+    # Test 1: Build a specified track
+    a = build_track()
+    test = create_segment_time_dict(a)
+    time = compute_track_time(a)
+    print_results(test, time)
+
+    # Test 2: Utilize the segment builder file
+    # c1 = segments.Straight(100, name="S1")
+    # c2 = segments.Corner(50, degree_90, name="C1")
+    # test.append(c1.to_dict())
+    # test.append(c2.to_dict())
+    # aga = create_segment_time_dict(test)
+    # time = compute_track_time(test)
+    # print(test)
+    # print_results(aga, time)
+
+    
+    #Test 3: Custom build a track based on what the user wants (Assuming they don't break the inputs)
+    # a = build_custom_track_for_user()
+    # print(a)
     # test = create_segment_time_dict(a)
     # time = compute_track_time(a)
     # print_results(test, time)
-    c1 = segments.Straight(100, name="S1")
-    c2 = segments.Corner(50, 90, name="C1")
-    test.append(c1.to_dict())
-    test.append(c2.to_dict())
-    aga = create_segment_time_dict(test)
-    time = compute_track_time(test)
-    print_results(aga, time)
-    # print(test)
-
-
-
 
 
 if __name__ == "__main__":
